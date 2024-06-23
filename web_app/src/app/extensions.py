954d8aaabc8d1ca591608c347727304da2ebdb04
Juan Esteban Garcia <juanesgarciamar@outlook.com>
2024-06-23 18:23:15 -0500

    feat: add flask database migration system

--- web_app/src/app/extensions.py
+++ web_app/src/app/extensions.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 from flask_marshmallow import Marshmallow
 from flask_wtf.csrf import CSRFProtect
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 class Base(DeclarativeBase):
     pass
@@ -15,3 +16,5 @@ ma = Marshmallow()
 csrf = CSRFProtect()
 
 jwt = JWTManager()
+
+migrate = Migrate()
