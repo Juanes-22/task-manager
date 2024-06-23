f3fef6710b5dd5532834a0c2f0d7f83df8ceb5d6
Juan Esteban Garcia <juanesgarciamar@outlook.com>
2024-06-23 16:03:56 -0500

    refactor: enhance project error handling

--- web_app/src/app/routes/errors.py
+++ web_app/src/app/routes/errors.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, current_app
 import traceback
+import logging
 
 from werkzeug.exceptions import HTTPException
 from marshmallow import ValidationError
@@ -14,53 +15,84 @@ from ..constants.http_status_codes import (
 )
 
 from ..extensions import db
-#from ..extensions import basic_auth
-from ..exceptions import BusinessError
+from ..extensions import jwt
 
+from ..exceptions import BusinessError
 
 errors_bp = Blueprint("errors", __name__)
 
 
+def log_error(exc, traceback: bool = False):
+    logging.error(f"Exception: {exc.__class__.__name__} {exc}")
+    if traceback:
+        traceback.print_exc()
+
+
+def get_error_message(exc, default_message):
+    if current_app.config["ENV"] != "production":
+        return f"{default_message}: {exc}"
+    return default_message
+
+
 @errors_bp.app_errorhandler(BusinessError)
 def handle_business_error(exc):
+    log_error(exc, traceback=True)
     return jsonify({"error": exc.message}), exc.status_code
 
 
 @errors_bp.app_errorhandler(HTTPException)
 def handle_http_exception(exc):
+    log_error(exc)
     return jsonify({"error": exc.description}), exc.code
 
 
 @errors_bp.app_errorhandler(Exception)
 def handle_unexpected_error(exc):
-    traceback.print_exc()
-    return jsonify({"error": f"Unexpected error: {exc}"}), HTTP_500_INTERNAL_SERVER_ERROR
+    log_error(exc, traceback=True)
+    error_message = get_error_message(exc, "An unexpected error occurred.")
+    return jsonify({"error": error_message}), HTTP_500_INTERNAL_SERVER_ERROR
 
 
 @errors_bp.app_errorhandler(HTTP_403_FORBIDDEN)
 def handle_forbidden(exc):
+    log_error(exc)
     return jsonify({"error": "Forbidden"}), exc.code
 
 
 @errors_bp.app_errorhandler(HTTP_404_NOT_FOUND)
 def handle_not_found(exc):
+    log_error(exc)
     return jsonify({"error": "Not found"}), exc.code
 
 
 @errors_bp.app_errorhandler(SQLAlchemyError)
 def handle_sqlalchemy_error(exc):
+    log_error(exc, traceback=True)
     db.session.rollback()
-    return (
-        jsonify({"error": f"Database error: {exc}"}),
-        HTTP_500_INTERNAL_SERVER_ERROR,
-    )
+    error_message = get_error_message(exc, "A database error occurred")
+    return jsonify({"error": error_message}), HTTP_500_INTERNAL_SERVER_ERROR
 
 
 @errors_bp.app_errorhandler(ValidationError)
 def handle_invalid_data(exc):
+    log_error(exc)
     return jsonify({"error": exc.messages}), HTTP_400_BAD_REQUEST
 
 
-# @basic_auth.error_handler
-# def unauthorized():
-#     return jsonify({"error": "Unauthorized access"}), HTTP_401_UNAUTHORIZED
+@jwt.invalid_token_loader
+def handle_invalid_token(exc):
+    log_error(exc)
+    error_message = get_error_message(exc, "Invalid token")
+    return jsonify({"error": error_message}), HTTP_400_BAD_REQUEST
+
+
+@jwt.unauthorized_loader
+def handle_missing_token(exc):
+    log_error(exc)
+    error_message = get_error_message(exc, "Unauthorized: missing token")
+    return jsonify({"error": error_message}), HTTP_401_UNAUTHORIZED
+
+
+@jwt.expired_token_loader
+def handle_expired_token(jwt_header, jwt_data):
+    return jsonify({"error": "Token expired"}), HTTP_400_BAD_REQUEST
