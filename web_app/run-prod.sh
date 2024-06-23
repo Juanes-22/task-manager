1bf69e23252d2e2cb1dc15d09ea85e17c62992cc
Juan Esteban Garcia <juanesgarciamar@outlook.com>
2024-06-23 18:25:10 -0500

    feat: add automation scripts for running the flask app and database migrations

--- web_app/run-prod.sh
+++ web_app/run-prod.sh
@@ -1,1 +1,12 @@
-gunicorn src.wsgi:app --bind 0.0.0.0:5000 --workers 4
+#!/bin/bash
+
+export FLASK_ENV=production
+
+echo "Setting up virtual environment..."
+poetry install --no-dev
+
+echo "Running database migrations..."
+poetry run flask db upgrade
+
+echo "Starting Gunicorn server..."
+poetry run gunicorn src.wsgi:app --bind 0.0.0.0:5000 --workers 4
